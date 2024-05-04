ad6cbf42506daf1c490150d82b7f7bb221e9a0f2
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-04 20:56:23 +0000

    Revert back

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,7 +10,7 @@ Contains:
     Fabfile to deploy/send created archive to web servers
 """
 import os
-from fabric.api import cd, env, put, run
+from fabric.api import cd, env, put, run, sudo
 
 
 env.hosts = ['34.224.2.49', '52.3.240.101']
@@ -22,16 +22,16 @@ def do_deploy(archive_path):
     """
     if not (archive_path and os.path.exists(archive_path)):
         return (False)
-    fname = ".".join(archive_path.split("/")[-1].split(".")[0:-1])
-    c1 = put(archive_path, "/tmp/{}.tgz".format(fname), use_sudo=True).succeeded
-    remote_path = "/data/web_static/releases/{}".format(fname)
-    c2 = run("mkdir -p {}".format(remote_path)).succeeded
-    c3 = run("tar -xzf /tmp/{} -C {}".format(fname, remote_path)).succeeded
-    c4 = run("mv {0}/web_static/* {0}".format(remote_path)).succeeded
-    c5 = run("rm -rf /tmp/{} {}/web_static".format(fname,
+    filename = archive_path.split("/")[-1].split(".")[0]
+    c1 = put(archive_path, "/tmp/{}".format(filename), use_sudo=True).succeeded
+    remote_path = "/data/web_static/releases/{}".format(filename)
+    c2 = sudo("mkdir -p {}".format(remote_path)).succeeded
+    c3 = sudo("tar -xzf /tmp/{} -C {}".format(filename, remote_path)).succeeded
+    c4 = sudo("mv {0}/web_static/* {0}".format(remote_path)).succeeded
+    c5 = sudo("rm -rf /tmp/{} {}/web_static".format(filename,
                                                     remote_path)).succeeded
     link = "/data/web_static/current"
-    c6 = run("ln -snf {} {}".format(remote_path, link)).succeeded
+    c6 = sudo("ln -snf {} {}".format(remote_path, link)).succeeded
     if c1 and c2 and c3 and c4 and c5 and c6:
         return (True)
     return (False)
