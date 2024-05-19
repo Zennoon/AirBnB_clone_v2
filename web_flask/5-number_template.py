2a21b9591f88030daa1f8bcb2b9b9d1133c84872
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-19 09:47:43 +0000

    Write a python script that starts a flask application with 6 routes defined, with one route returning output from a template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""
+
+Contains:
+    Misc
+    ====
+    Script to initialize a flask application with 6 routes defined, listening
+    on 0.0.0.0:5000
+
+    Functions
+    =========
+    home - Route function for the '/' url
+
+    hbnb - Route function for the '/hbnb' url
+
+    c_path - Route function for '/c/<text>' url
+
+    python_path - Route function for '/python/<text>' url
+
+    number_path - Route function for '/number/<num>' url
+
+    number_template - Route function for the '/number_template/<num>' url
+"""
+from flask import Flask, render_template
+
+
+app = Flask("__name__", template_folder="./web_flask/templates")
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """Route function for the '/' url"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Route function for the '/hbnb' url"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>')
+def c_path(text):
+    """
+    Route function for urls matching '/c/<text>'
+
+    Args:
+        text (str): path following '/c/' in the URL fed to the function
+
+    """
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_path(text="is cool"):
+    """
+    Route function for urls matching '/python/<text>'
+
+    Args:
+        text (str): path following '/python/' in the url,
+                    default value='is cool'
+    """
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route('/number/<int:num>')
+def number_path(num):
+    """
+    Route function for urls matching '/number/<num>' where num is a positive
+    integer
+
+    Args:
+        num (int): path following '/number/', converted to an int if possible
+    """
+    return ("{} is a number".format(num))
+
+
+@app.route('/number_template/<int:num>')
+def number_template(num):
+    """
+    Route function for urls matching '/number_template/<num>' where num is a
+    positive integer
+
+    Args:
+        num (int): path following '/number_template/', converted to an int
+    """
+    return (render_template("5-number.html", num=num))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
