58771364a803daa546f39a0077ef55dd615607f2
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-19 08:11:45 +0000

    Write a python script to start a web application with 2 routes defined

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+
+Contains:
+    Misc
+    ====
+    Script to initialize a flask application with two routes defined, listening
+    on 0.0.0.0:5000
+
+    Functions
+    =========
+    home - Route function for the '/' url
+
+    hbnb - Route function for the '/hbnb' url
+"""
+from flask import Flask
+
+
+app = Flask("__name__")
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """Route function for the '/' url"""
+    return ("Hello HBNB!")
+
+@app.route('/hbnb')
+def hbnb():
+    """Route function for the '/hbnb' url"""
+    return ("HBNB")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
