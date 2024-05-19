7a545b411ffb55adeda2caec3c99b58b030c38a5
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-05-19 14:39:01 +0000

    Write a script that starts a web app with one route defined that displays all states sorted by their name

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+
+Contains:
+    Misc
+    ====
+    Script to initialize a flask web app with a single route to display
+    the States in the database
+
+    Functions
+    =========
+    states_list - Route function for '/states_list' url
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask("__name__", template_folder="./web_flask/templates")
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def renew_session(exc):
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    all_states = storage.all(State).values()
+    return (render_template("7-states_list.html", all_states=all_states))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
